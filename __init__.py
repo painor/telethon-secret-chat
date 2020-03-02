56d0cfd6d23a60b4aef10bde55a7959914ed6cc6
painor <30215770+painor@users.noreply.github.com>
2020-03-02 19:01:11 +0100

    Add init and version

---
+++ __init__.py
@@ -0,0 +1,7 @@
+from .secret_sechma import secretTL
+from . import version
+from secret_chat_manager import SecretChatManager
+
+__version__ = version.__version__
+
+__all__ = ['secretTL', 'SecretChatManager']
