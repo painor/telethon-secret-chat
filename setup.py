1f106e7d871485a2330dc0207cba4ad27d4419e5
painor <30215770+painor@users.noreply.github.com>
2020-03-02 19:46:36 +0100

    Update version and pypi

--- setup.py
+++ setup.py
@@ -1,13 +1,17 @@
+import re
 from distutils.core import setup
 from os import path
 this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
+with open('telethon_secret_chat/version.py', 'r', encoding='utf-8') as f:
+    version = re.search(r"^__version__\s*=\s*'(.*)'.*$",
+                        f.read(), flags=re.MULTILINE).group(1)
 
 setup(
     name='telethon_secret_chat',
     packages=['telethon_secret_chat'],
-    version='0.1.2',
+    version=version,
     license='MIT',
     description='Telethon secret chat plugin',
     author='painor',
@@ -23,11 +27,8 @@ setup(
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
-        'Topic :: Software Development :: Build Tools',
+        'Topic :: Communications :: Chat',
         'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
+        "Programming Language :: Python :: 3",
     ],
 )
