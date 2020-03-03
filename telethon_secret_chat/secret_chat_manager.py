61f9d001ceaf0534c572efdfb3cccd1489be3516
painor <30215770+painor@users.noreply.github.com>
2020-03-03 17:40:09 +0100

    Fix patching tlobjects Fix undefined updates

--- telethon_secret_chat/secret_chat_manager.py
+++ telethon_secret_chat/secret_chat_manager.py
@@ -2,14 +2,19 @@ from enum import Enum
 
 from telethon import TelegramClient
 from telethon.tl import types
+from telethon.tl.alltlobjects import tlobjects
 
-from telethon_secret_chat.secret_sechma import secret_tlobjects
+from .secret_sechma import secret_tlobjects
 from .secret_methods import SecretChatMethods
 
 
 class SECRET_TYPES(Enum):
     accept = 1
     decrypt = 2
+
+
+def patch_tlobjects():
+    tlobjects.update(secret_tlobjects)
 
 
 class SecretChatManager(SecretChatMethods):
@@ -21,7 +26,6 @@ class SecretChatManager(SecretChatMethods):
         self.dh_config = None
         self.auto_accept = auto_accept
         self.client = client
-        self.patch_tlobjects()
         self.client.add_event_handler(self._secret_chat_event_loop)
 
     def add_secret_event_handler(self, event_type=SECRET_TYPES.decrypt, func=None):
@@ -29,10 +33,6 @@ class SecretChatManager(SecretChatMethods):
             raise ValueError("Wrong params")
         # deal with patterns etc
         self.secret_events.append((event_type, func))
-
-    def patch_tlobjects(self):
-        from telethon.tl.alltlobjects import tlobjects
-        tlobjects.update(secret_tlobjects)
 
     def patch_event(self, event):
 
@@ -47,6 +47,9 @@ class SecretChatManager(SecretChatMethods):
         event.response = respond
 
     async def _secret_chat_event_loop(self, event):
+        if 0x1be31789 not in tlobjects:  # check for decryptedMessage constructor
+            patch_tlobjects()   # patch the tlobjects so we can read it with bytes
+
         if isinstance(event, types.UpdateEncryption):
             if isinstance(event.chat, types.EncryptedChat):
                 await self.finish_secret_chat_creation(event.chat)
@@ -54,9 +57,9 @@ class SecretChatManager(SecretChatMethods):
                 if self.auto_accept:
                     await self.accept_secret_chat(event.chat)
                     return
-                for event in self.secret_events:
-                    (event, callback) = event
-                    if event == SECRET_TYPES.accept:
+                for events in self.secret_events:
+                    (event_type, callback) = events
+                    if event_type == SECRET_TYPES.accept:
                         self.client.loop.create_task(callback(event))
         elif isinstance(event, types.UpdateNewEncryptedMessage):
             decrypted_event = None
@@ -65,5 +68,7 @@ class SecretChatManager(SecretChatMethods):
                 if event_type == SECRET_TYPES.decrypt:
                     if not decrypted_event:
                         decrypted_event = await self.handle_encrypted_update(event)
+                        if decrypted_event is None:
+                            return
                         self.patch_event(decrypted_event)
                     self.client.loop.create_task(callback(decrypted_event))
